9be4c7ea69fe81dbc4bd8d7493fa272492a417cb
tjb803 <tim.baldwin@tinymail.co.uk>
2012-05-17 17:04:52 +0000

    Remove PTS 'view' - it isn't very useful

--- src/elliott803/view/ComputerView.java
+++ src/elliott803/view/ComputerView.java
@@ -43,16 +43,16 @@ public class ComputerView extends JDesktopPane implements ActionListener {
         this.computer = computer;
 
         // Create views for the various devices
+        // Note: PtsView not currently displayed as it does not seem very useful!  
         console = new ConsoleView(computer.console, this);
         cpu = new CpuView(computer.cpu);
         store = new StoreView(computer.core);
         pts = new PtsView(computer.pts);
         plotter = new PlotterView(computer.plotter);
-        
+
         add(console);
         add(cpu);
         add(store);
-        add(pts);
         add(pts.reader[0]);
         add(pts.reader[1]);
         add(pts.punch[0]);
@@ -83,19 +83,16 @@ public class ComputerView extends JDesktopPane implements ActionListener {
         int maxX = Math.max(max1X, max2X);
 
         int max1Y = console.getHeight() + pts.teletype.getHeight() + plotterY + 10;
-        int max2Y = cpu.getHeight() + 2*pts.punch[0].getHeight() + pts.getHeight() + 15;
-        int max3Y = store.getHeight() + 2*pts.reader[0].getHeight() + pts.getHeight() + 15;
+        int max2Y = cpu.getHeight() + 2*pts.punch[0].getHeight() + 15;
+        int max3Y = store.getHeight() + 2*pts.reader[0].getHeight() + 15;
         int maxY = Math.max(Math.max(max1Y, max2Y), max3Y);
 
         int consoleX = 0, consoleY = 0;
         int storeX = maxX - store.getWidth(), storeY = 0;
         int cpuX = storeX - cpu.getWidth() - 10, cpuY = 0;
 
-        pts.setSize(pts.reader[0].getWidth() + pts.punch[0].getWidth() + 5, pts.getHeight());
-
         int teletypeX = 0, teletypeY = maxY - pts.teletype.getHeight();
-        int ptsX = maxX - pts.getWidth(), ptsY = maxY - pts.getHeight();
-        int punch2X = maxX - pts.punch[1].getWidth(), punch2Y = ptsY - pts.punch[1].getHeight();
+        int punch2X = maxX - pts.punch[1].getWidth(), punch2Y = maxY - pts.punch[1].getHeight();
         int reader2X = punch2X - pts.reader[1].getWidth() - 5, reader2Y = punch2Y;
         int punch1X = punch2X,  punch1Y = punch2Y - pts.punch[0].getHeight();
         int reader1X = reader2X, reader1Y = reader2Y - pts.reader[0].getHeight();
@@ -108,7 +105,6 @@ public class ComputerView extends JDesktopPane implements ActionListener {
         plotter.setLocation(teletypeX + plotterX, teletypeY - plotterY);
 
         pts.teletype.setLocation(teletypeX, teletypeY);
-        pts.setLocation(ptsX, ptsY);
         pts.punch[0].setLocation(punch1X, punch1Y);
         pts.punch[1].setLocation(punch2X, punch2Y);
         pts.reader[0].setLocation(reader1X, reader1Y);
